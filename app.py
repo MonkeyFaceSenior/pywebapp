d32ad3bde75a2cac8aad4aeaab2fda8c58ec832d
MONKEYbutt193 <hanna@lau.fm>
2024-10-04 18:37:44 +1000

    websites

---
+++ app.py
@@ -0,0 +1,28 @@
+# main page in flask diary app
+import sqlite3
+from flask import Flask, render_template
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('./flask_blog/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close() 
+    #print('hello testing')
+    return render_template('index.html', posts=posts)
