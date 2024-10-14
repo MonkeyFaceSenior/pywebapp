09a57e1ec5c359bfae23cf2a553aeeb437247d38
MonkeyFaceSenior <shiuming@lau.fm>
2024-10-14 20:29:47 +1100

    updated the flaskenv to use the correct app name and ip address to bind to .. also added a small batch file to start flask app for my notebook

---
+++ runme.bat
@@ -0,0 +1,3 @@
+#shortcut to run on my notebook
+
+C:\Users\Shiuming\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\flask.exe -e flaskenv.txt run
