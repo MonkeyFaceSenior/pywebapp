01bf44c776b4f9d1fdc7b804bc19d6b6fd9128c4
MonkeyFaceSenior <shiuming@lau.fm>
2025-01-20 17:51:47 +1100

    update to runme.bat

--- runme.bat
+++ runme.bat
@@ -1,3 +1,3 @@
-#shortcut to run on my notebook
+rem shortcut to run on my notebook
 
-C:\Users\Hanna\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\flask.exe -e flaskenv.txt run
+%APPDATA%\..\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\flask.exe -e flaskenv.txt run
